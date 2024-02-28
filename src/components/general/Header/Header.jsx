505ebad13b8db66367396907af24e71efae7e9f7
rodyherrera <contact@rodyherrera.com>
2024-02-27 22:50:56 -0300

    Removal of unused dependency

--- src/components/general/Header/Header.jsx
+++ src/components/general/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
-import { useInView } from 'react-intersection-observer';
+import { ScrollTrigger } from 'gsap/all';
 import GreenCircleAnim from '@components/general/GreenCircleAnim';
 import './Header.css';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Header = () => {
-    const [isInViewRef, isInView] = useInView({ threshold: 0.5 });
-    const [animationPlayed, setAnimationPlayed] = useState(false);
     const [localTime, setLocalTime] = useState(new Date().toLocaleTimeString());
     const headerItems = [
         ['Rodolfo', 'Herrera H.'],
@@ -17,7 +17,9 @@ const Header = () => {
     ];
 
     useEffect(() => {
-        if(!isInView || animationPlayed) return;
+        const intervalId = setInterval(() => {
+            setLocalTime(new Date().toLocaleTimeString());
+        }, 1000);
         gsap.fromTo('.Header-Item-Container', {
             opacity: 0,
             y: -20
@@ -26,20 +28,17 @@ const Header = () => {
             opacity: 1,
             y: 0,
             stagger: 0.2,
-            ease: 'power2.out'
+            ease: 'power2.out',
+            scrollTrigger: {
+                trigger: '.Header-Container',
+                top: 'start 80%'
+            }
         });
-        setAnimationPlayed(true);
-    }, [isInView, animationPlayed]);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setLocalTime(new Date().toLocaleTimeString());
-        }, 1000);
         return () => clearInterval(intervalId);
     }, []);
 
     return (
-        <header className='Header-Container' ref={isInViewRef}>
+        <header className='Header-Container'>
             <section className='Header-Wrapper-Container'>
                 {headerItems.map(([ top, bottom, Icon ], index) => (
                     <article className='Header-Item-Container' key={index}>
